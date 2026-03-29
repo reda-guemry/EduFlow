b44e3acbcc85af48fdd2c295f3c07d4a3168f094
reda-guemry <guemryreda@gmail.com>
2026-03-29 11:36:54 +0000

    modifier for test

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+
+test('un utulusateur peut se connecter avec des identifiants valides', function () {
+    $user = User::factory()->create([
+        'first_name' => 'Test',
+        'last_name' => 'User',
+        'email' => 'test@example.com' , 
+        'password' => bcrypt('password') , 
+        'role' => 'student',
+    ]);
+
+    $reponse = $this -> postJson('/api/login' , [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]) ; 
+
+    $reponse -> assertStatus(200) ; 
+
+});
+
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,2 @@
 <?php
 
-test('the application returns a successful response', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
-});
